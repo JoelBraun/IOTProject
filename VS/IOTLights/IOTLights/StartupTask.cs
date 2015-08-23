557af6e0572a64f966034f8c4f2b433bd4ee5ca6
Joel Braun <Joelbraun@outlook.com>
2015-08-22 18:54:47 -0700

    Added VS Projects
    Added EventHub capable code to an IOT project, also added a universal
    project.

--- VS/IOTLights/IOTLights/StartupTask.cs
+++ VS/IOTLights/IOTLights/StartupTask.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
@@ -13,9 +14,40 @@ namespace IOTLights
     {
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to start one or more asynchronous methods 
-            //
+            // Generate a SAS key with the Signature Generator.: https://github.com/sandrinodimattia/RedDog/releases
+            var sas = "SharedAccessSignature sr=https%3a%2f%2fjoeliot.servicebus.windows.net%2fhub1%2fpublishers%2ftestdev1%2fmessages&sig=miOuWFNgj2FNfEbCaVMGGS5qNGtOdApiQ7gQAwDR8wQ%3d&se=1440652646&skn=Policy1";
+
+            // Namespace info.
+            var serviceNamespace = "joeliot";
+            var hubName = "hub1";
+            var deviceName = "TestDev1";
+
+            // Create client.
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress =
+                       new Uri(String.Format("https://{0}.servicebus.windows.net/", serviceNamespace));
+            httpClient.DefaultRequestHeaders
+                       .TryAddWithoutValidation("Authorization", sas);
+
+
+            // Keep sending.
+            while (true)
+            {
+                var eventData = new
+                {
+                    Temperature = new Random().Next(20, 50)
+                };
+
+                var postResult = httpClient.PostAsJsonAsync(
+                       String.Format("{0}/publishers/{1}/messages", hubName, deviceName), eventData).Result;
+
+
+                //Thread.Sleep(new Random().Next(1000, 5000));
+
+                Task.Delay(new Random().Next(1000, 5000));
+
+
+            }
         }
     }
 }
