e4ce03fc5948bd4066b7a60220833cd3432c3fa1
Joel Braun <Joelbraun@outlook.com>
2015-08-29 22:44:39 -0700

    Codebase Merge
    Merged Codebase with other project

--- VS/IOTLightsUniversal/IOTLightsUniversal/MainPage.xaml.cs
+++ VS/IOTLightsUniversal/IOTLightsUniversal/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using IOTLightsUniversal.Common;
+using IOTLightsUniversal.Data;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,18 +24,37 @@ namespace IOTLightsUniversal
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
         public MainPage(Frame frame)
         {
             this.InitializeComponent();
             MainSplitView.Content = frame;
             (MainSplitView.Content as Frame).Navigate(typeof(HomePage));
+            getData();
+        }
+
+        private async void getData()
+        {
+            var sampleDataGroups = await SampleDataSource.GetGroupsAsync();
+            this.DefaultViewModel["Groups"] = sampleDataGroups;
         }
 
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
             MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
         }
-
+        /*
         private void Item1Click(object sender, RoutedEventArgs e)
         {
             (MainSplitView.Content as Frame).Navigate(typeof(HomePage));
@@ -47,5 +68,6 @@ namespace IOTLightsUniversal
             MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
             HamburgerList.SelectedItem = Microphone;
         }
+        */
     }
 }
